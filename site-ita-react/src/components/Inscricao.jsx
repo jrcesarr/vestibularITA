de97a3471bc05144111d0a3e592466b8a6bbc62a
Cesar Noronha <cesarn.junior2@gmail.com>
2026-04-15 16:40:35 -0300

    trilha concluida

--- site-ita-react/src/components/Inscricao.jsx
+++ site-ita-react/src/components/Inscricao.jsx
@@ -5,8 +5,8 @@ function Inscricao(){
     <section className="insc_container">
         {/* Grid Brilhante*/}
         {/* animationDelay para eles não piscarem todos ao mesmo tempo */}
-        <div className="glow-box" style={{ top: '200px', left: '200px', animationDelay: '0s' }}></div>
-        <div className="glow-box" style={{ top: '300px', right: '150px', animationDelay: '2s' }}></div>
+        <div className="glow-box" style={{ top: '150px', right: '200px', animationDelay: '0s' }}></div>
+        <div className="glow-box" style={{ bottom: '40px', right: '150px', animationDelay: '2s' }}></div>
         <div className="glow-box" style={{ bottom:  '50px', left: '100px', animationDelay: '4s' }}></div>
         <div className="glow-box" style={{ top: '50px', left: '500px', animationDelay: '1.5s' }}></div>
 
@@ -15,8 +15,59 @@ function Inscricao(){
             <h3>Veja o passo a passo da jornada do vestibular!</h3>
        
         </div>
+
         <div id="trilha-secao">
+            <div className="trilha-passos">
+                
+                {/* Passo 1 */}
+                <div className="passo">
+                    <div className="passo-header">
+                        <span className="passo-numero">1</span>
+                        <h4 className="passo-titulo">INSCRIÇÃO</h4>
+                    </div>
+                    <p>Preencha seus dados e confirme a inscrição. Lembre-se do prazo de pagamento do boleto!</p>
+                </div>
+
+                <div className="seta-fluxo">&gt;</div>
+                
+                {/* Passo 2 */}
+                <div className="passo">
+                    <div className="passo-header">
+                        <span className="passo-numero">2</span>
+                        <h4 className="passo-titulo">INSTRUÇÕES</h4>
+                    </div>
+                    <p>Posteriormente, você receberá as instruções da primeira fase no e-mail, incluindo data e local de prova.</p>
+                </div>
+
+                <div className="seta-fluxo">&gt;</div>
+
+                {/* Passo 3 */}
+                <div className="passo">
+                    <div className="passo-header">
+                        <span className="passo-numero">3</span>
+                        <h4 className="passo-titulo">RESULTADOS</h4>
+                    </div>
+                    <p>Após a primeira fase, a lista de classificados para a 2ª fase e o local de prova serão disponibilizados.</p>
+                </div>
+
+                <div className="seta-fluxo">&gt;</div>
+
+                {/* Passo 4 */}
+                <div className="passo">
+                    <div className="passo-header">
+                        <span className="passo-numero">4</span>
+                        <h4 className="passo-titulo">CONVOCAÇÃO</h4>
+                    </div>
+                    <p>Os aprovados na segunda fase serão convocados para a realização da inspeção de saúde em São José dos Campos.</p>
+                </div>
+
+            </div>
+
+            <button className="btn-iniciar-jornada">
+                QUERO FAZER MINHA INSCRIÇÃO
+            </button>
         </div>
+
     </section>
     );
 }
