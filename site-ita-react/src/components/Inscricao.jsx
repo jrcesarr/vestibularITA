4e8cba9843d91a3f96390f9b866a070fc0966ecb
Cesar Noronha <cesarn.junior2@gmail.com>
2026-04-15 10:04:34 -0300

    grid interativo

--- site-ita-react/src/components/Inscricao.jsx
+++ site-ita-react/src/components/Inscricao.jsx
@@ -3,6 +3,13 @@ import './Inscricao.css';
 function Inscricao(){
  return (
     <section className="insc_container">
+        {/* Grid Brilhante*/}
+        {/* animationDelay para eles não piscarem todos ao mesmo tempo */}
+        <div className="glow-box" style={{ top: '200px', left: '200px', animationDelay: '0s' }}></div>
+        <div className="glow-box" style={{ top: '300px', right: '150px', animationDelay: '2s' }}></div>
+        <div className="glow-box" style={{ bottom: '50px', left: '350px', animationDelay: '4s' }}></div>
+        <div className="glow-box" style={{ top: '50px', left: '500px', animationDelay: '1.5s' }}></div>
+
         <div className="insc_titulo">
             <h1>TRILHA PARA A INSCRIÇÃO NO ITA</h1>
             <h3>Veja o passo a passo da jornada do vestibular!</h3>
